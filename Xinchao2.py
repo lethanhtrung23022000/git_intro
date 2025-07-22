26bce9745881f639522adace626e2eb7dbbc2a9c
Lê Trung <letrung@MacBook-Pro-2.local>
2025-07-22 10:20:02 +0700

    resolve conflict

--- Xinchao2.py
+++ Xinchao2.py
@@ -1,3 +1,6 @@
 print("xin chào lan 2")
 
-print("tao thay doi de pulll git. Tao them conflic")
+print("tao thay doi de pulll git, thu xem co bi conflict ko")
+
+
+print("thu xem co bi conflict ko")
